2320ad54c59d0a1c23d90591a413ac31409e1617
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-19 16:12:46 -0300

    feat: criar model de Sala

---
+++ SalaFacil/app/src/main/java/com/fatec/salafacil/domain/sala/enum/RecursoSala.kt
@@ -0,0 +1,10 @@
+package com.fatec.salafacil.domain.sala.enum
+
+enum class RecursoSala {
+    PROJETOR,
+    QUADRO_BRANCO,
+    AR_CONDICIONADO,
+    WIFI,
+    TELEVISAO,
+    VIDEO_CONFERENCIA
+}
