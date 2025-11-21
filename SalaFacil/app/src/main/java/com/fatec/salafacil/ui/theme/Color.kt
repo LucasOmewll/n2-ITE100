030b544a79d53b9077d8b7399d7a92b969840a17
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-21 20:22:54 -0300

    feat: configurar cores da aplicação

--- SalaFacil/app/src/main/java/com/fatec/salafacil/ui/theme/Color.kt
+++ SalaFacil/app/src/main/java/com/fatec/salafacil/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.fatec.salafacil.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Base
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Primary Brand Colors
+val Brand100 = Color(0xFFCCE5FF)
+val Brand200 = Color(0xFF66B2FF)
+val Brand300 = Color(0xFF3299FF)
+val Brand400 = Color(0xFF007FFF)
+val Brand500 = Color(0xFF0066CC)
+
+// Greyscale
+val Grey100 = Color(0xFF212529)
+val Grey200 = Color(0xFF525252)
+val Grey300 = Color(0xFFADB5BD)
+val Grey400 = Color(0xFFE9ECEF)
+val Grey500 = Color(0xFFF8F9FA)
+
+// State Colors
+val ErrorColor = Color(0xFFBA1A1A)
+val SuccessColor = Color(0xFF2E7D32)
+val WarningColor = Color(0xFFF57C00)
+val InfoColor = Color(0xFF0066CC)
