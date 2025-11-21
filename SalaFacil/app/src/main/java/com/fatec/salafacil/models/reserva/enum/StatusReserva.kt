497434d46b350bd36e03eb9187349a0c6bef1212
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-21 12:42:34 -0300

    refactor: refazer projeto usando jetpack compose

--- SalaFacil/app/src/main/java/com/fatec/salafacil/models/reserva/enum/StatusReserva.kt
+++
@@ -1,7 +0,0 @@
-package com.fatec.salafacil.models.reserva.enum
-
-enum class StatusReserva {
-    ATIVA,
-    CANCELADA,
-    CONCLUIDA
-}
--- SalaFacil/app/src/main/java/com/fatec/salafacil/models/usuario/enums/TipoUsuario.kt
+++
@@ -1,7 +0,0 @@
-package com.fatec.salafacil.models.usuario.enums
-
-enum class TipoUsuario {
-    GUEST,
-    AUTHENTICATED_USER,
-    ADMIN
-}
