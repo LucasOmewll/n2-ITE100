ae6eed1ce85ef5fb63a0e289557d3a94546b8c1e
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-22 15:03:37 -0300

    feat: criar arquivos de telas de rotas

---
+++ SalaFacil/app/src/main/java/com/fatec/salafacil/ui/routes/Routes.kt
@@ -0,0 +1,11 @@
+package com.fatec.salafacil.ui.routes
+
+object AppRoutes {
+    const val WELCOME_SCREEN = "welcome_screen"
+    const val LOGIN = "login"
+    const val ONBOARDING = "onboarding"
+    const val HOME = "home"
+    const val EDIT_MEETING = "edit_meeting"
+    const val EDIT_ROOM = "edit_room"
+    const val BOOK_ROOM = "book_room"
+}
