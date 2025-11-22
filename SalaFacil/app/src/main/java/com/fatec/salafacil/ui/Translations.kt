147ccc3a39e1e123b4eb43a6527bff1cd5c0986f
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-22 18:38:04 -0300

    feat: criar tela de welcome

---
+++ SalaFacil/app/src/main/java/com/fatec/salafacil/ui/Translations.kt
@@ -0,0 +1,7 @@
+package com.fatec.salafacil.ui
+
+object PT {
+    val welcome_title = "SalaFácil"
+    val welcome_subtitle = "Facilite o agendamento de salas de reunião."
+    val welcome_start_button = "Comece agora"
+}
