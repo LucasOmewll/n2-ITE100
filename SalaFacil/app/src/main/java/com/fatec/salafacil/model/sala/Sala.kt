f777093e0dac42052df25055dac9b3ff77ad8aed
Lucas Gomes <lucasgomes.luc@gmail.com>
2025-11-22 13:58:24 -0300

    feat: criar model de sala

---
+++ SalaFacil/app/src/main/java/com/fatec/salafacil/model/sala/Sala.kt
@@ -0,0 +1,18 @@
+package com.fatec.salafacil.model.sala
+
+import com.google.firebase.Timestamp
+import java.util.UUID
+
+data class Sala(
+    val id: UUID = UUID.randomUUID(),
+    var nome: String = "",
+    var endereco: String = "",
+    var capacidade: Int = 0,
+    var imageUrl: String = "",
+    var hasProjector: Boolean = false,
+    var hasWhiteboard: Boolean = false,
+    var hasAirConditioning: Boolean = false,
+    var hasWifi: Boolean = false,
+    var hasVideoConference: Boolean = false,
+    val createdAt: Timestamp = Timestamp.now()
+)
